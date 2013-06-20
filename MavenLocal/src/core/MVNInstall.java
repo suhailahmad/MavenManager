1e9f08378a7c689b01a3e50d1f44de515494e695
Anirudh Varma <anirudh@fhlabs.in>
2013-06-20 14:02:22 +0530

    First commit on GitHub

---
+++ MavenLocal/src/core/MVNInstall.java
@@ -0,0 +1,36 @@
+package core;
+
+import java.io.File;
+
+/**
+ *
+ * @author Anirudh Varma, Ashish Dandriyal
+ */
+public class MVNInstall {
+    
+    public boolean installFile(File file,String[] param){
+		boolean status=false;		
+		String path=file.getAbsolutePath();
+		String command=generateCommand(path,param[0],param[1],param[2]);
+		Runtime runtime=Runtime.getRuntime();
+		try{
+		Process pr=runtime.exec(command);
+		pr.waitFor();
+		return true;
+		}
+		catch(Exception e){
+			System.out.println(e.getLocalizedMessage());
+			return status;
+		}		
+	}
+	
+	private static String generateCommand(String filePath, String artifact, String group, String version){
+		String space=" ";
+		String dfile="-Dfile="+filePath+space;
+		String dartifact="-DartifactId="+artifact+space;
+		String dgroup="-DgroupId="+group+space;
+		String dversion="-Dversion="+version+space;		
+		String command="mvn install:install-file"+space+dfile+dgroup+dartifact+dversion+"-Dpackaging=jar";
+		return command;
+	}
+}
